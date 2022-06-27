c11292860e9c4407d886f17547ad77ace1ca514a
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-06-27 01:22:03 +0000

    order_detail before

---
+++ db/migrate/20220627011959_create_order_details.rb
@@ -0,0 +1,13 @@
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_details do |t|
+      t.references :order,      null: false, foreign_key: true
+      t.references :item,       null: false, foreign_key: true
+      t.integer :price,         null: false
+      t.integer :amount,        null: false
+      t.integer :making_status, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
