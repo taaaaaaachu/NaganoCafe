c7c50214033b6f5ab2d1e49ccbbd72afe1cf6749
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-06-27 01:04:55 +0000

    Item after

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_003915) do
+ActiveRecord::Schema.define(version: 2022_06_27_010119) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -54,5 +54,17 @@ ActiveRecord::Schema.define(version: 2022_06_27_003915) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_items_on_genre_id"
+  end
+
   add_foreign_key "addresses", "customers"
+  add_foreign_key "items", "genres"
 end
