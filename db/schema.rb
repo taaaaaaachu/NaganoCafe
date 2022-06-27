b86788ffd1113ce18bdad26f7e27b6de3461ecee
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-06-27 01:10:49 +0000

    cartitem after

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_010554) do
+ActiveRecord::Schema.define(version: 2022_06_27_010950) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 2022_06_27_010554) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "customer_id", null: false
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -72,5 +82,7 @@ ActiveRecord::Schema.define(version: 2022_06_27_010554) do
   end
 
   add_foreign_key "addresses", "customers"
+  add_foreign_key "cart_items", "customers"
+  add_foreign_key "cart_items", "items"
   add_foreign_key "items", "genres"
 end
