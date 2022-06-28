1266f1bc75eadc07beb39ede5dbfdc99b909ceb2
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-06-28 07:26:12 +0000

    cart_item

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-  has_many :orders,     dependent: :destroy
+  has_many :orders
   has_many :addresses,  dependent: :destroy
 
   # Include default devise modules. Others available are:
