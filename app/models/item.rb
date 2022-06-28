156c82ee68ed411571d6c6938e8e6c1513fa9a5a
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-06-28 07:47:35 +0000

    genre & finish association

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :genre
-  has_many :customers, through: :cart_item
+  has_many :customers, through: :cart_items
   has_many :order_details
 
   has_one_attached :image
