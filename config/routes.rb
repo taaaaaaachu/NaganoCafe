cd6f490f452292eb2e9f64e399f268608a18d452
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-07-06 03:12:29 +0000

    routes edit namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,71 +1,43 @@
 Rails.application.routes.draw do
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about', to: 'homes#about'
+    resources :addresses, except: [:show, :new]
+    resources :orders, only: [:index, :show, :new, :create] do
+      collection do
+        post :comfirm
+        get :complete
+      end
+    end
+    resources :cart_items, only: [:index, :update, :destroy, :create] do
+      collection do
+        delete :destroy_all
+      end
+    end
+    resources :customers, only: [:show, :edit, :update] do
+      collection do
+        get :unsubscribe
+        patch :withdraw
+      end
+    end
+    resources :items, only: [:index, :show]
+  end
+
   namespace :admin do
-    get 'order_details/update'
+    root to: 'homes#top'
+    resources :order_details, only: :update
+    resources :orders, only: [:show, :update]
+    resources :customers, except: [:destroy, :create, :new]
+    resources :genres, except: [:destroy, :show, :new]
+    resources :items, except: :destroy
   end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/comfirm'
-    get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+
+
+
+
+
+
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
